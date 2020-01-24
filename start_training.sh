2fe68800e75b80e2ced0dd31b6fa82b758c81cb5
Felix Feldmann <felix@bnbit.de>
2020-01-24 14:29:54 +0100

    AnimalPose: Split config files.

--- start_training.sh
+++ start_training.sh
@@ -1,20 +1,13 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=9
+#export CUDA_VISIBLE_DEVICES=9
 
-BASE_CONFIGS="FlowFrameGen/configs/Human36MFramesFlowMeta.yaml FlowFrameGen/configs/VUnet.yaml FlowFrameGen/configs/train.yaml"
-TARGET_FRAME_STEP=25
-WARPING="none"
+BASE_CONFIGS="AnimalPose/configs/single_cat_pose.yaml AnimalPose/configs/VUnet.yaml AnimalPose/configs/train.yaml"
 
-name="VUnet_s_"$TARGET_FRAME_STEP"_w_"$WARPING"_wsin_if_wosin_t"
-kwargs="-t --target_frame_step "$TARGET_FRAME_STEP" --warping "$WARPING
-# command="python AnimalPose/scripts/load_config.py -n "$name" -b "$BASE_CONFIGS" "$kwargs
-# echo $command
-# $command
+name="AnimalPose_VUnet"
+kwargs="-t "
 
-#python AnimalPose/scripts/load_config.py -b $BASE_CONFIGS $kwargs
 command="edflow -n "$name" -b "$BASE_CONFIGS" "$kwargs
 echo $command
 echo CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
-which python
 $command
