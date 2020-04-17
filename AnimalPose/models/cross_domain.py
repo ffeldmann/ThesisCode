a19ddb28ea125dd585ea01c1cc7a1ef8e245b3d1
Felix Feldmann <felix@bnbit.de>
2020-04-17 23:23:48 +0200

    Adds dummy for Cross domain paper

---
+++ AnimalPose/models/cross_domain.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+class CrossDomain(nn.Module):
+    def __init__(self):
+        super(CrossDomain, self).__init__()
+        pass
+
+    def forward(self):
+        pass
