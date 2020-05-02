6ef1a48ba0727b4522c87ec8857480a03a4590a4
Felix Feldmann <felix@bnbit.de>
2020-05-02 16:43:26 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as f:
 
 setup(
     name="AnimalPose",
-    version="0.0.1",
-    author="Hannes Perrot",
-    author_email="hp96@gmx.de",
-    description="Next video frame generation with optical flow prior",
+    version="1.0.0",
+    author="Felix Feldmann",
+    author_email="felix@bnbit.de",
+    description="Animal Pose Estimation",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ffeldman/AnimalPose",
